29a6208c208203e081f2f12a4ea9bd0e3b13e815
ballaprr <ballaprr@oregonstate.edu>
2025-05-11 16:30:46 -0700

    dimensionality 25

--- node.lua
+++ node.lua
@@ -19,8 +19,8 @@ local QR_POSITION_CONFIG = {
     position = "custom",
     
     -- Dimensions
-    width = 101,   -- Width of QR code area
-    height = 101,  -- Height of QR code area
+    width = 25,   -- Width of QR code area
+    height = 25,  -- Height of QR code area
     
     -- Margin from screen edges
     margin = 40,
