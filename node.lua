3cb72f445f53a4dc989f824dc08f644503a257ba
gsamva <gsam@courtsidevideo.com>
2026-07-01 09:05:01 -0400

    restored the new assets

--- node.lua
+++ node.lua
@@ -2630,14 +2630,14 @@ local coke_overlay = {
     preloaded_images = {},
     logos = {
         ["1"] = "Courtside_logo.png",
-        ["2"] = "Coke_Zero_Revised_1_lowres.png"
---        ["2"] = "Coke_Zero_Revised_1_lowres.png",
---		["3"] = "transparent_2160x.png",
---		["4"] = "transparent_red.png",
---       ["5"] = "Courtside_Logo_2160x.png",
---        ["6"] = "Philadelphia_76ers_logo_PhillyVerses_2160x_shadow.png",
---        ["7"] = "Converse_Logo.wine_2160x.png",
---        ["8"] = "CURE_Auto_Insurance_logo_2160x.png"
+--        ["2"] = "Coke_Zero_Revised_1_lowres.png"
+        ["2"] = "Coke_Zero_Revised_1_lowres.png",
+		["3"] = "transparent_2160x.png",
+		["4"] = "transparent_red.png",
+		["5"] = "Courtside_Logo_2160x.png",
+        ["6"] = "Philadelphia_76ers_logo_PhillyVerses_2160x_shadow.png",
+        ["7"] = "Converse_Logo.wine_2160x.png",
+        ["8"] = "CURE_Auto_Insurance_logo_2160x.png"
 
     }
 }
@@ -3319,8 +3319,16 @@ local function ExternalEvents()
 		["logo/set"] = function(data)
 			local logo_name = data and data ~= "" and data or "Courtside_logo.png"
 			
-			if logo_name == "Courtside_logo.png" or logo_name == "Coke_Zero_Revised_1_lowres.png" then
-			load_coke_overlay(logo_name)
+--			if logo_name == "Courtside_logo.png" or logo_name == "Coke_Zero_Revised_1_lowres.png" then
+			if 	logo_name == "Courtside_logo.png" or 
+				logo_name == "Coke_Zero_Revised_1_lowres.png" or
+				logo_name == "transparent_2160x.png" or
+				logo_name == "transparent_red.png" or
+				logo_name == "Courtside_Logo_2160x.png" or
+				logo_name == "Philadelphia_76ers_logo_PhillyVerses_2160x_shadow.png" or
+				logo_name == "Converse_Logo.wine_2160x.png" or
+				logo_name == "CURE_Auto_Insurance_logo_2160x.png" then
+				load_coke_overlay(logo_name)
 				log("logo_switch", "Logo set to: %s", logo_name)
 			else
 				log("logo_switch", "Invalid logo: %s", logo_name)
