e79cb934085d72cde89f41789e5879a27443f1c4
gsamva <gsam@courtsidevideo.com>
2026-07-12 21:14:22 -0700

    changed default rotation to 270 for the Name List

--- node.lua
+++ node.lua
@@ -264,7 +264,8 @@ local function Screen()
     end)
 
     node.event("config_updated", function(config)
-		rotation = config.rotation
+--rotation = config.rotation
+		rotation = 270
         local is_portrait = rotation == 90 or rotation == 270
         local width, height = config.resolution[1], config.resolution[2]
         log("screen", "configured content resolution is %dx%d", width, height)
